8e747377ab99501c316c0f2faa1634d45ba67c5e
MohamedGoush <0x1110111@gmail.com>
2023-10-28 21:10:17 +0530

    Initial Commit

---
+++ WhatsUp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WhatsUp
+//
+//  Created by python on 28/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by python on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
